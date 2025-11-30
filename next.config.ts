409fbbb2ece23dfda997e65e1df4cd5c4706d3f7
Guoxuan Xu <guoxuan.xu8@gmail.com>
2025-11-30 15:57:29 -0800

    docker setup; version control; prisma config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // standalone build for Docker
 };
 
 export default nextConfig;
